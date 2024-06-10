237efdae32f12afa66b3319a2bf82568030ba9a6
DonDomenico <d.grunow@hotmail.de>
2024-06-10 18:37:14 +0200

    created skills section

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  icons: {source: string, text: string}[] = [
+    {source: './assets/img/angular-logo.png', text: 'Angular'},
+    {source: './assets/img/ts-logo.png', text: 'TypeScript'},
+    {source: './assets/img/js-logo.png', text: 'JavaScript'},
+    {source: './assets/img/html-logo.png', text: 'HTML'},
+    {source: './assets/img/firebase-logo.png', text: 'Firebase'},
+    {source: './assets/img/git-logo.png', text: 'GIT'},
+    {source: './assets/img/css-logo.png', text: 'CSS'},
+    {source: './assets/img/api-logo.png', text: 'Rest-Api'},
+    {source: './assets/img/scrum-logo.png', text: 'Scrum'},
+  ]
+    
+  
 }
