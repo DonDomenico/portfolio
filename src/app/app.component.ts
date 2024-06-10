708d6c6ba6432c3a225d08d707b27ed8dae661e8
DonDomenico <d.grunow@hotmail.de>
2024-06-10 16:10:58 +0200

    added blurry background-images to atf and aboutMe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import { MyportfolioComponent } from './myportfolio/myportfolio.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AtfComponent, AboutmeComponent, MyportfolioComponent, ContactComponent, FooterComponent, MainPageComponent],
+  imports: [RouterOutlet, HeaderComponent, AtfComponent, AboutmeComponent, MyportfolioComponent, ContactComponent, FooterComponent, MainPageComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
