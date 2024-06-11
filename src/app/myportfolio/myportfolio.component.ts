a30de3824e4270d88553fec20ba716e28188c3b3
DonDomenico <d.grunow@hotmail.de>
2024-06-11 17:10:46 +0200

    skills section complete. Portfolio section started

--- src/app/myportfolio/myportfolio.component.ts
+++ src/app/myportfolio/myportfolio.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+interface project {
+  title: string;
+  tools: string[];
+  description: string;
+  img: string;
+  liveTest: string;
+  github: string;
+}
+
 @Component({
   selector: 'app-myportfolio',
   standalone: true,
@@ -8,5 +17,14 @@ import { Component } from '@angular/core';
   styleUrl: './myportfolio.component.scss'
 })
 export class MyportfolioComponent {
-
+  projects: project[] = [
+    {
+      title: 'Join',
+      tools: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      img: './assets/img/join.png',
+      liveTest: 'link',
+      github: 'link'
+    }
+  ]
 }
