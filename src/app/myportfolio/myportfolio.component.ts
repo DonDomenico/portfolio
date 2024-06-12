7637451d3dd2a8c31a6993da318fe1beaa20066c
DonDomenico <d.grunow@hotmail.de>
2024-06-12 11:50:37 +0200

    finalized portfolio section

--- src/app/myportfolio/myportfolio.component.ts
+++ src/app/myportfolio/myportfolio.component.ts
@@ -21,10 +21,26 @@ export class MyportfolioComponent {
     {
       title: 'Join',
       tools: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
-      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      description: 'Task manager inspired by the Kanban System. Create and organize\ntasks using drag and drop functions, assign users and categories.',
       img: './assets/img/join.png',
-      liveTest: 'link',
-      github: 'link'
+      liveTest: 'https://dominik-grunow.developerakademie.net/join/',
+      github: 'https://github.com/DonDomenico/join'
+    },
+    {
+      title: 'El Pollo Loco',
+      tools: ['JavaScript', 'HTML', 'CSS'],
+      description: 'Jump, run and throw game based on object-oriented approach.\nHelp the character Pepe to find coins and tabasco salsa to fight againt the crazy hen.',
+      img: './assets/img/pollo-loco.png',
+      liveTest: 'https://dominik-grunow.developerakademie.net/pollo_loco/',
+      github: 'https://github.com/DonDomenico/pollo-loco'
+    },
+    {
+      title: 'Pokédex',
+      tools: ['JavaScript', 'HTML', 'CSS', 'Api'],
+      description: 'A simple library that provides and catalogues pokémon\ninformation based on the PokéAPI.',
+      img: './assets/img/pokedex.png',
+      liveTest: 'https://dominik-grunow.developerakademie.net/pokedex/index.html',
+      github: 'https://github.com/DonDomenico/pokedex'
     }
   ]
 }
