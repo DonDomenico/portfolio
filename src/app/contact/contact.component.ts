3de10af5966b0a2cada5b191d3dec9cb2e5a00c0
DonDomenico <d.grunow@hotmail.de>
2024-06-13 18:53:53 +0200

    added events to form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule,],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  checkbox = true;
 
   http = inject(HttpClient)
 
@@ -33,21 +36,30 @@ export class ContactComponent {
   };
 
   onSubmit(NgForm: NgForm) {
-    if (NgForm.submitted && NgForm.form.valid && !this.mailTest) {
-      this.http.post(this.post.endPoint, this.post.body(this.contactData))
-        .subscribe({
-          next: (response) => {
+    // if (NgForm.submitted && NgForm.form.valid && !this.mailTest) {
+    //   this.http.post(this.post.endPoint, this.post.body(this.contactData))
+    //     .subscribe({
+    //       next: (response) => {
 
-            NgForm.resetForm();
-          },
-          error: (error) => {
-            console.error(error);
-          },
-          complete: () => console.info('send post complete'),
-        });
-    } else if (NgForm.submitted && NgForm.form.valid && this.mailTest) {
+    //         NgForm.resetForm();
+    //       },
+    //       error: (error) => {
+    //         console.error(error);
+    //       },
+    //       complete: () => console.info('send post complete'),
+    //     });
+    // } else if (NgForm.submitted && NgForm.form.valid && this.mailTest) {
 
-      NgForm.resetForm();
+    //   NgForm.resetForm();
+    // }
+    console.log('Test');
+  }
+
+  toggleCheckboxState() {
+    if(this.checkbox) {
+      this.checkbox = false;
+    } else {
+      this.checkbox = true;
     }
   }
 }
--- src/app/myportfolio/myportfolio.component.ts
+++ src/app/myportfolio/myportfolio.component.ts
@@ -22,7 +22,7 @@ export class MyportfolioComponent {
       title: 'Join',
       tools: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
       description: 'Task manager inspired by the Kanban System. Create and organize\ntasks using drag and drop functions, assign users and categories.',
-      img: './assets/img/join.png',
+      img: './assets/img/join-new.png',
       liveTest: 'https://dominik-grunow.developerakademie.net/join/',
       github: 'https://github.com/DonDomenico/join'
     },
@@ -30,7 +30,7 @@ export class MyportfolioComponent {
       title: 'El Pollo Loco',
       tools: ['JavaScript', 'HTML', 'CSS'],
       description: 'Jump, run and throw game based on object-oriented approach.\nHelp the character Pepe to find coins and tabasco salsa to fight againt the crazy hen.',
-      img: './assets/img/pollo-loco.png',
+      img: './assets/img/pollo-loco-new.png',
       liveTest: 'https://dominik-grunow.developerakademie.net/pollo_loco/',
       github: 'https://github.com/DonDomenico/pollo-loco'
     },
@@ -38,7 +38,7 @@ export class MyportfolioComponent {
       title: 'Pokédex',
       tools: ['JavaScript', 'HTML', 'CSS', 'Api'],
       description: 'A simple library that provides and catalogues pokémon\ninformation based on the PokéAPI.',
-      img: './assets/img/pokedex.png',
+      img: './assets/img/pokedex-new.png',
       liveTest: 'https://dominik-grunow.developerakademie.net/pokedex/index.html',
       github: 'https://github.com/DonDomenico/pokedex'
     }
