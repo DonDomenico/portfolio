5d2b01d840582598f4568f5c85cf7d2340f84391
DonDomenico <d.grunow@hotmail.de>
2024-06-24 17:14:24 +0200

    translation and responsive css

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,16 +1,19 @@
-import { CommonModule, NgStyle, NgTemplateOutlet } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { FormsModule, NgForm, NgModel, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, NgForm, NgModel } from '@angular/forms';
+import { TranslationService } from '../translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, TranslateModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  translate = inject(TranslationService);
   colorDisabled = '#B6B6B6';
   colorEnabled = '#00BEE8';
   cursorEnabled = 'pointer';
