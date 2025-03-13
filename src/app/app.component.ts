764fad9e5d583d2f3a971c405a2a13f602984d3a
DonDomenico <d.grunow@hotmail.de>
2025-03-13 18:21:15 +0100

    fixed some vulnerabilities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { AtfComponent } from './atf/atf.component';
-import { AboutmeComponent } from './aboutme/aboutme.component';
-import { MyportfolioComponent } from './myportfolio/myportfolio.component';
-import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { SkillsComponent } from './skills/skills.component';
-import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AtfComponent, AboutmeComponent, MyportfolioComponent, ContactComponent, FooterComponent, MainPageComponent, SkillsComponent, MainContentComponent],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
